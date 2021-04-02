da7b98c6cfb92bdb3ee7c8a7f0e84c98d396a29e
pomaretta <cpomaresp@gmail.com>
2021-04-02 11:44:48 +0200

    Added new routes

--- server/routes/api/vbox.js
+++ server/routes/api/vbox.js
@@ -9,6 +9,7 @@
 // IMPORTS
 
 import { Router } from 'express'
+import axios from 'axios'
 import virtualbox from 'virtualbox'
 
 // INIT
@@ -32,16 +33,86 @@ vbox.get('/api/machines/list', (req,res) => {
 
 // MACHINE
 
+// IP
 vbox.get('/api/machines/:id/ip',(req,res) => {
-    
     let options = {
         vm: req.params.id,
         key: '/VirtualBox/GuestInfo/Net/0/V4/IP'
     }
-
     virtualbox.guestproperty.get(options,(value,error) => {
         if(error) throw error
-        res.json(value)
+        res.json({
+            vm: req.params.id,
+            ip: value
+        })
+    })
+})
+
+// OS
+vbox.get('/api/machines/:id/os',(req,res) => {
+    let options = {
+        vm: req.params.id,
+        key: '/VirtualBox/GuestInfo/OS/Product'
+    }
+    virtualbox.guestproperty.get(options,(value,error) => {
+        if(error) throw error
+        res.json({
+            vm: req.params.id,
+            os: value
+        })
+    })
+})
+
+// NAME
+vbox.get('/api/machines/:id/name',(req,res) => {
+    let vm = req.params.id
+    virtualbox.list((machines,error) => {
+        Object.entries(machines).forEach(([key,value]) => {
+            if(vm == key){
+                res.json({
+                    vm: vm,
+                    name: value.name,
+                })
+            }
+        })
+    })
+})
+
+// STATE
+vbox.get('/api/machines/:id/state',(req,res) => {
+    let vm = req.params.id
+    virtualbox.list((machines,error) => {
+        Object.entries(machines).forEach(([key,value]) => {
+            if(vm == key){
+                res.json({
+                    vm: vm,
+                    state: value.running,
+                })
+            }
+        })
+    })
+})
+
+// INFO
+vbox.get('/api/machines/:id/info',async (req,res) => {
+
+    let vm = req.params.id
+    let name = await axios.get(`http://localhost:8000/api/machines/${vm}/name`)
+    let os = await axios.get(`http://localhost:8000/api/machines/${vm}/os`)
+    let state = await axios.get(`http://localhost:8000/api/machines/${vm}/state`)
+    let ip = null
+    
+    if(state.data.state){
+        ip = await axios.get(`http://localhost:8000/api/machines/${vm}/ip`)
+        ip = ip.data.ip
+    }
+
+    res.json({
+        vm: vm,
+        name: name.data.name,
+        os: os.data.os,
+        ip: ip,
+        state: state.data.state
     })
     
 })
@@ -59,6 +130,11 @@ vbox.get('/api/machines/:id/ip',(req,res) => {
     FUNCTIONALITIES
 
     -- LIST VMACHINES
+    -- GET MACHINE BASIC INFO
+        - NAME
+        - OS
+        - IP
+        - STATE
 
 */
 
