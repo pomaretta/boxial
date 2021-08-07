4b1517fe017f1a35c14301dd8e9c25d6d660a671
Carlos Pomares <cpomaresp@gmail.com>
2021-08-07 17:32:28 +0200

    Working on a new version of Boxial Server

---
+++ server/src/components/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+app = express();
+
+// ======================== //
+// MIDDLEWARES              //
+// ======================== //
